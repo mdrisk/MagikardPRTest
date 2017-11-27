ef5be7aa5b68023c8ef87c8459e64a4308cbb432
mdrisk <mrisk@westclarkschools.com>
2017-11-27 10:43:59 -0500

    I made changes

--- app/src/main/java/com/example/mannnl/magikard/MainActivity.java
+++ app/src/main/java/com/example/mannnl/magikard/MainActivity.java
@@ -14,6 +14,8 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    //This is my change!
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
